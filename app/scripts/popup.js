eafcfd4b7fe6dd15efaab0133c705cbbd4818ac8
MikeESL <mike.reser.03@gmail.com>
2014-05-03 12:38:37 -0400

    confusion

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -1,3 +1,17 @@
-'use strict';
+// 'use strict';
 
-console.log('\'Allo \'Allo! Popup');
+// console.log('\'Allo \'Allo! Popup');
+$(document).ready(function(){
+
+	$JSON("http://www.reddit.com/r/homebrewing.json", function(data){
+
+		//var HomeBrewData = data;
+		var TemplateString = _.template($("#redditHomebrew".html(), data);
+		
+		
+		$(".brewData").html(TemplateString);
+
+		
+			)
+	})
+})//pixel resort
