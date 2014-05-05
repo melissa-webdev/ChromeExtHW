0027454e07b4d5f680fb1abf28dfb44ee0e193fc
MikeESL <mike.reser.03@gmail.com>
2014-05-05 14:25:57 -0400

    styling adjustments

--- app/scripts/popup.js
+++ app/scripts/popup.js
@@ -20,14 +20,43 @@ $(function(){
 		var html = " ";
 			for(var i=0, b=brewData.length; i<b; i++){
 				var obj = brewData[i].data;
+				var thumbnail = obj.thumbnail;
+					if (obj.thumbnail === "self")
+						thumbnail = 'images/beer.png';
 				var title = obj.title;
 				var url = "http://www.reddit.com" + obj.permalink;	
-				html += '<li><a href=\"'+url+'\" target=\"_blank\"><p>'+title+'</p></a></li>';
+				html += '<li><img src=\" '+ thumbnail +' \"><a href=\"'+url+'\" target=\"_blank\"><p>'+title+'</p></a></li>';
 			};
 
 		$(".brew").html(html);		
 });
 });
+
+// tryig something more difficult
+// $(function(){
+// 	$("#brewForm").on('submit', function(event){
+// 		event.preventDefault ();
+
+// 		var searchData = $("#brew").val();
+// 		var searchUrl = "http://www.reddit.com/r/Homebrewing/search?q=" + searchData + "&restrict_sr=on"
+// 	})
+// 	$.getJSON("http://www.reddit.com/r/Homebrewing.json", function(data){
+// 		var brewData = data.data.children;
+// 		var html = " ";
+// 			for(var i=0, b=brewData.length; i<b; i++){
+// 				var obj = brewData[i].data;
+// 				var thumbnail = obj.thumbnail;
+// 					if (obj.thumbnail === "self")
+// 						thumb = 'images/beer.png';
+// 				var title = obj.title;
+// 				var url = "http://www.reddit.com" + obj.permalink;	
+// 				html += '<li><img src=\" '+ thumbnail +' \"><a href=\"'+url+'\" target=\"_blank\"><p>'+title+'</p></a></li>';
+// 			};
+
+// 		$(".brew").html(html);		
+// });
+// });
+
 
 });
 
